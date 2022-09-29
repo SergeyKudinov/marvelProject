86398ef7b55c6c92fda64ddd4d5040a7c9226825
Sergey <serezha.kudinov.1995@mail.ru>
2022-09-29 12:47:27 +0300

    add functional to get random character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,9 +1,7 @@
-
-
 class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=2d801555d7b07b6b785eafd05070ad2b'
+    _apiKey = 'apikey=2d801555d7b07b6b785eafd05070ad2b';
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -14,11 +12,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return ({
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        })
     }
 }
 
